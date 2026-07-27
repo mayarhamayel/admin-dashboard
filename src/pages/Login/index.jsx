348670c1cf8e58a5d56902edba00028c58b2d7f2
mayarhamayel <mayargit4@gmail.com>
2026-07-27 15:53:51 +0300

    Create admin login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,78 @@
+import { Form, Input, Button, message } from "antd";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    const { email, password } = values;
+
+    
+    if (email === "admin@gmail.com" && password === "123456") {
+      localStorage.setItem("isLoggedIn", "true");
+
+      message.success("Login Successfully");
+
+      navigate("/dashboard");
+    } else {
+      message.error("Invalid Email or Password");
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin Login</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white w-full max-w-md p-8 rounded-xl shadow-md">
+        <h1 className="text-3xl font-bold text-center mb-2">Admin Login</h1>
+
+        <p className="text-gray-500 text-center mb-8">
+          Welcome back, please login to your account
+        </p>
+
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+              {
+                type: "email",
+                message: "Enter a valid email",
+              },
+            ]}
+          >
+            <Input placeholder="Enter your email" size="large" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your password",
+              },
+              {
+                min: 6,
+                message: "Password must be at least 6 characters",
+              },
+            ]}
+          >
+            <Input.Password placeholder="Enter your password" size="large" />
+          </Form.Item>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            className="w-full"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
